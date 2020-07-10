ee9fd40c29b7c774a304688516ebb604f347b61e
runali.ambavkar <runa197@gmail.com>
2020-07-10 16:36:28 +0530

    second Data Commit

--- www/PaymentUrway.js
+++ www/PaymentUrway.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'PaymentUrway', 'coolMethod', [arg0]);
+exports.payment = function (arg0, success, error) {
+    exec(success, error, 'PaymentUrway', 'payment', [arg0]);
 };
