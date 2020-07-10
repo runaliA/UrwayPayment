07ddb86c6a88155a9d4c007b6d4702516df8820f
runali.ambavkar <runa197@gmail.com>
2020-07-10 19:52:58 +0530

    forth Commit

--- www/PaymentUrway.js
+++ www/PaymentUrway.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.payment = function (arg0, success, error) {
+module.exports.payment = function (arg0, success, error) {
     exec(success, error, 'PaymentUrway', 'payment', [arg0]);
 };
